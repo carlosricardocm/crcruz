5d009bcdee7720e6da951434e268baff080fe3b7
carlosricardocm <cenatraciencias@gmail.com>
2019-03-20 09:56:28 -0600

    se agrego la practica 2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# crcruz
+Introducción a Ciencias de la Computación
+
+Almacén con plantillas para prácticas y ejercicios.
+
+Notas:
+
+Puedes clonar mi almacén y ejecutar $git pull para tener la última actualización.
---
+++ practicas/src/icc/practica02/Main.java
@@ -0,0 +1,28 @@
+pakage icc.practica01;
+
+/**
+ * Clase con el método principal
+ *
+ * author
+ * version
+ */
+public class main {
+
+    /**
+     * Método principal
+     * @param args
+     */
+    public static void main(string []args){
+
+	//Declaración de variables
+	String mensaje = "Corrigiendo código"
+	int i = uno;
+	char a = "b";
+	
+	System.out.println("mensaje"); //Linea que imprime el valor de la variable mensaje
+	System.out.println("El valor de i es " + i);
+	System.out.println("El caracter almacenado es " + a);
+	
+    }
+}
+
